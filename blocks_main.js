0d51f6d2d60ec477895279f880cf0b0ac3fbdeef
satoyami <satoyami@yahoo.com>
2025-03-05 19:49:39 -0800

    blocks seq 1

--- blocks_main.js
+++ blocks_main.js
@@ -10,6 +10,8 @@ import createBlocks from './create-blocks.js';
 // ########    SETUP    #######
 // ############################
 
+
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
